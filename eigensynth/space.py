4a12f5f58404bc0b35bbde8129a0a8e57cfc8714
Gregor Corbin <gregor.corbin@posteo.de>
2026-02-16 20:25:34 +0100

    Compute eigenfunctions, eigenvalues of Laplace-1D

--- eigensynth/space.py
+++ eigensynth/space.py
@@ -1,3 +1,30 @@
 # SPDX-FileCopyrightText: None
 #
 # SPDX-License-Identifier: CC0-1.0
+import numpy as np
+
+
+def laplace_1d_eigen(x, N, L=1.):
+    """
+    Eigenfunctions and eigenvalues of the 1D Laplace operator ( c^2 d_xx u)
+    on the interval [0,L] with zero Dirichlet boundary conditions, i.e.
+
+    u_k(x) such that c^2 d_xx u_k = lambda_k * u_k, k = 1, ..., N
+
+    evaluated at x.
+
+    These are given by
+
+    u_k(x) = sqrt(L/2) * sin( k * pi / L * x),
+    lambda_k = -(k * pi / L)**2
+
+    where the factor sqrt(L/2) is chosen to normalize the eigenfunctions.
+
+    :param x: Positions at which u_k are evaluated.
+    :param N: Number of eigenvectors and eigenvalues
+    :param L: Length of the domain
+    :return: (u_k, lambda_k). u_k is an array with shape (N, x.size), lambda_k is a vector of length N
+    """
+    k = np.linspace(1, N, N)
+    arg = k * np.pi / L
+    return np.sqrt( L / 2.) * np.sin(np.atleast_2d(arg).transpose() * np.atleast_2d(x)), -1. * arg**2
--- mein-eigener-synthesizer.py
+++ mein-eigener-synthesizer.py
@@ -3,9 +3,14 @@
 # SPDX-License-Identifier: CC0-1.0
 from eigensynth import writer
 from eigensynth.time import oscillator, damped_oscillator, damped_oscillator_coefficients, c2_from_frequency
+from eigensynth.space import laplace_1d_eigen
 
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+def initial_condition(x0, x):
+    return np.where(x<x0,(1-x0)*x, x0*(1.-x))
 
 
 if __name__ == '__main__':
@@ -13,21 +18,32 @@ if __name__ == '__main__':
     dur = 2  # seconds
     halflife = 0.2  # seconds
     base_freq = 880  # Hz
-    freq = np.atleast_2d(np.array([base_freq, base_freq*np.pow(2., 3./12.), base_freq*np.pow(2., 7./12.)]))
+    L = 1.  # m
+    Nx = 101  # Discretize x with Nx values
+    Nk = 20  # Use Nk eigenfunctions
+
+    x = np.linspace(0., L, Nx)
+    e_k, lam_k = laplace_1d_eigen(x, N=Nk, L=L)
+
+    U0 = initial_condition(0.5, x)  # Initial condition, evaluated at x
+    u0_k = np.matmul(e_k, U0)  # Coefficients in expansion of initial condition U0 = sum_k u0_k * e_k(x)
+
+    #freq = np.atleast_2d(np.array([base_freq, base_freq*np.pow(2., 3./12.), base_freq*np.pow(2., 7./12.)]))
 
     t = np.atleast_2d(np.arange(0, dur, 1./samplerate)).transpose()
-    c2 = c2_from_frequency(freq)
-    x = oscillator(t, c2, 1., 0.)
 
-    c2_dampened, d = damped_oscillator_coefficients(freq, halflife)
-    xd = damped_oscillator(t, c2_dampened, d, 1., 0.)
+    c2, d = damped_oscillator_coefficients(base_freq, halflife)
+    u_k = damped_oscillator(t, -4. * L**2 * base_freq * lam_k, d, u0_k, 0.)
+    U = np.matmul(u_k, e_k)
 
-    print(f'harmonic c^2 = {c2}, dampened c^2 = {c2_dampened}, dampened d = {d}')
+    U_x50 = np.matmul(u_k, e_k[:, 50])
 
-    N = 1000
-    plt.plot(t[0:N], np.sum(x[0:N], axis=1), label='harmonic oscillator')
-    plt.plot(t[0:N], np.sum(xd[0:N], axis=1), label='dampened oscillator')
+    print(f'base c^2 @ 880 Hz = {c2}, d = {d}')
+    print(f'actual c^2 = {-4. * L**2 * base_freq**2 * lam_k}')
+
+    N = 4000
+    plt.plot(t[0:N], U[0:N, 50], label='oscillator x = 0.5')
+    plt.plot(t[0:N], np.sum(U[0:N, :], axis=1), label='oscillator, integrated')
     plt.legend()
     plt.show()
-    writer.write_soundfile('harmonic_880Hz', np.sum(x, axis=1), samplerate)
-    writer.write_soundfile('dampened_880Hz', np.sum(xd, axis=1), samplerate)
+    writer.write_soundfile('string', U_x50, samplerate)
