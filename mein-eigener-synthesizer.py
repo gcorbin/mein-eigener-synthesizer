ab1f7769e3716ac35be1a80a414ac864f9d436d8
Gregor Corbin <gregor.corbin@posteo.de>
2026-02-21 13:08:03 +0100

    Implement basic input loop with pynput

--- mein-eigener-synthesizer.py
+++ mein-eigener-synthesizer.py
@@ -2,11 +2,31 @@
 #
 # SPDX-License-Identifier: CC0-1.0
 from argparse import ArgumentParser
+from pynput import keyboard
+
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+        if key is keyboard.Key.enter:
+            print('received enter, stopping')
+            return False
 
 
 def main():
     parser = ArgumentParser(description="Play sounds on key presses")
     args = parser.parse_args()
+
+    listener = keyboard.Listener(
+        on_press=on_press,
+        suppress=True)
+
+    listener.start()
+    listener.join()
 
 
 if __name__ == '__main__':
