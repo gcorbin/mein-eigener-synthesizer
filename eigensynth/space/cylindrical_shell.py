Fix cylindrical_shell eigenvalue computation

eigensynth/space/cylindrical_shell.py
@@ -7,6 +7,8 @@ from numpy.typing import NDArray
 
 from eigensynth.array import outer_product_nd
 
+__all__ = ['cylindrical_shell_eigen']
+
 
 def cylindrical_shell_eigen(x: tuple[NDArray, NDArray], N: tuple[int, int], L: tuple[float, float],
                             shell_constant=5.e5):
@@ -35,9 +37,9 @@ def cylindrical_shell_eigen(x: tuple[NDArray, NDArray], N: tuple[int, int], L: t
     m = np.repeat(np.arange(1, N_z + 1, 1, dtype=int), N_theta)  # slow index
     n = np.tile(np.arange(1, N_theta + 1, 1, dtype=int), N_z)  # fast index
 
-    lam_k = -1. * (np.power(np.pi * m / L_z, 4) + 2. + np.power(np.pi / L_z / a * m * n, 2) + np.power(n / a, 4))
+    lam_k = -1. * (np.power(np.pi * m / L_z, 4) + 2. * np.power(np.pi / L_z / a * m * n, 2) + np.power(n / a, 4))
     kappa_k = np.power(m * np.pi / L_z, 2)
-    gamma_k = lam_k + shell_constant * np.power(a, -4) * np.power(kappa_k, 2) * np.power(lam_k, -1)
+    gamma_k = lam_k + shell_constant / np.power(a, 4) * np.power(kappa_k, 2) / lam_k
 
     # For each (m,n) there are two eigenfunctions: cos(n * theta ) and sin(n * theta)
     eZ = np.sin(outer_product_nd(Z, m) * np.pi / L_z)

pipe-visualization.py
@@ -3,9 +3,15 @@
 # SPDX-License-Identifier: CC0-1.0
 from argparse import ArgumentParser
 import matplotlib.pyplot as plt
+import sounddevice
 from matplotlib import cm
 import numpy as np
+
+from eigensynth.plot import plot_sound_signal, plot_sound_spectrum
+from eigensynth.sounds import play_sound, normalize
 from eigensynth.space.cylindrical_shell import cylindrical_shell_eigen
+from eigensynth.instruments import Pipe, PipeOptions
+from eigensynth.time import samples, damped_oscillator_coefficients, damped_oscillator, oscillator_frequency
 
 
 def main():
@@ -16,36 +22,71 @@ def main():
 
 
 def visualize_initial_condition():
-    L = 2
-    a = 0.5
-    shell_constant = 5e5
-    Nx = [100,100]
-    z = L * np.linspace(0., 1., Nx[0] + 1)
-    phi = 2. * np.pi * np.linspace(0., 1., Nx[1] + 1)
-    Z, Phi = np.meshgrid(z, phi)
+    a = 1.
+    L = np.pi*a
+    shell_constant = 1e4
 
-    (m,n) = (20,20)
-    e_k, gamma_k = cylindrical_shell_eigen((Z,Phi), (m,n), (L,a), shell_constant)
+    (m,n) = (12,12)
 
-    forcing_point = np.meshgrid(np.array([0.8*L]), np.array([0.25 *  np.pi]))
-    e0, _ = cylindrical_shell_eigen(forcing_point, (m,n), (L,a), shell_constant)
+    forcing_point = np.meshgrid(np.array([0.7*L]), np.array([0.25 *  np.pi]))
+    e0, gamma_k = cylindrical_shell_eigen(forcing_point, (m,n), (L,a), shell_constant)
     c0 = e0.reshape(-1) / gamma_k
-    w0 = np.sum(c0.reshape(1,1,-1) * e_k, axis=2)
 
-    #print(gamma_k.reshape(m,n))
-    print(np.amax(w0))
-    R = a * np.ones_like(Z) +  0.1 / np.amax(w0) * w0
-    X = R * np.cos(Phi)
-    Y = R * np.sin(Phi)
+    samplerate = 44100.
+    base_frequency = 440.
+    duration = 2.
+    halflife = duration / 10.
 
-    # Plot the surface
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, facecolors=cm.Blues(R/np.amax(R)), rcount=100, ccount=100)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
+    ks, kd = damped_oscillator_coefficients(base_frequency, halflife)
+    tuned_gamma = -ks / np.min(np.abs(gamma_k)) * gamma_k
 
+    # Show eigenvalues gamma_mn as (m,n) matrix
+    print(f"ks = {ks}, kd = {kd}")
+    print(f"base mode: {np.argmin(tuned_gamma)}, freq = {oscillator_frequency(np.min(tuned_gamma))}")
 
+    """plt.matshow(tuned_gamma[0:m*n].reshape((m,n)))
+    plt.xlabel('n')
+    plt.ylabel('m')
+    plt.show()"""
+
+    t = samples(samplerate, duration)
+
+    u_k = damped_oscillator(
+        t=t,
+        k=tuned_gamma,
+        d=kd,
+        x0=c0,
+        dx0=0.)
+
+    sound = normalize(np.sum(np.inner(u_k, e0), axis=(1,2)), scale=4.)
+    play_sound(sound, samplerate, blocking=False, mode='clip', device='pipewire')
+
+    fig, (ax1, ax2) = plt.subplots(2,1)
+    plot_samples = int(100. / base_frequency * samplerate)
+    plot_sound_signal(ax1, t[0:plot_samples], sound[0:plot_samples], label='sound')
+    plot_samples = int(1 * samplerate)
+    plot_sound_spectrum(ax2, sound[0:plot_samples], samplerate, base_frequency=base_frequency, label='sound')
     plt.show()
+
+    """Nx = [100,100]
+        z = L * np.linspace(0., 1., Nx[0] + 1)
+        phi = 2. * np.pi * np.linspace(0., 1., Nx[1] + 1)
+        Z, Phi = np.meshgrid(z, phi)
+        e_k, gamma_k = cylindrical_shell_eigen((Z,Phi), (m,n), (L,a), shell_constant)
+        w0 = np.sum(c0.reshape(1,1,-1) * e_k, axis=2)
+        print(np.amax(w0))
+        R = a * np.ones_like(Z) +  0.1 / np.amax(w0) * w0
+        X = R * np.cos(Phi)
+        Y = R * np.sin(Phi)
+
+        # Plot the surface
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, facecolors=cm.Blues(R/np.amax(R)), rcount=100, ccount=100)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+
+        plt.show()"""
+
 
 
 def parse_command_line_args():